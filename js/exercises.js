da5a2e2a06b831e1fde77c684de556a2e32e132a
Yerkebulan <example@gmail.com>
2022-10-27 14:38:11 +0600

    getting elements from page

---
+++ js/exercises.js
@@ -0,0 +1,24 @@
+"use strict"
+
+let x = 5;
+alert(x++);
+
+console.log([] + false - null + true);
+
+let y = 2;
+let z = y = 3;
+alert(z);
+
+console.log([] + 1 + 2);
+
+console.log(2 && 1 && null && 0 && undefined);
+
+let a,b;
+console.log(!!( a && b ));
+console.log(( a && b ));
+
+alert( "1"[0] );
+
+alert( null || 2 && 3 || 4 );
+alert( +"Infinity" );
+console.log("ааааа" > "яблоко");
